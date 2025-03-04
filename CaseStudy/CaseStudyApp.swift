6a38d8ea22dab302a401b5b3773da2b6dc651ed5
Alperen KARACA <alperenkaraca89@gmail.com>
2025-03-04 14:30:48 +0300

    Initial Commit

---
+++ CaseStudy/CaseStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaseStudyApp.swift
+//  CaseStudy
+//
+//  Created by Alperen KARACA on 4.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct CaseStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
